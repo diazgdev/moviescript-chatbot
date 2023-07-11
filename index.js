433565e25a9eed4eca4e5923cd50608537ce53f3
Guillermo Díaz <diazgdev@192.168.0.192>
2023-07-11 00:50:19 -0600

    remove env import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { process } from '/env'
+// import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
 const setupTextarea = document.getElementById('setup-textarea')
